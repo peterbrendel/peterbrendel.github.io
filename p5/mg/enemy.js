6c82504bc1dc0a6c7578f761e391936748e912a9
Peter Brendel <peter_brendel@hotmail.com>
2017-10-09 00:47:08 -0300

    minigame3

--- p5/mg/enemy.js
+++ p5/mg/enemy.js
@@ -2,11 +2,16 @@ class Enemy {
 
 	constructor(x){
 		this.x = x;
-		this.y = height-height/10 - 25;
+		this.y = height-height/10-5;
 	}
 
 	move() {
 		this.x--;
+	}
+
+	draw(){
+		fill(255,0,0);
+		ellipse(this.x, this.y, 25, 25);
 	}
 
 
--- p5/mg/sketch.js
+++ p5/mg/sketch.js
@@ -2,23 +2,30 @@ let maxHeight;
 let speed = 15;
 let player;
 let canvas;
+let p;
 
 function setup() {
 
 	canvas = createCanvas(windowWidth/2, windowHeight/2);
 	canvas.position((windowWidth - width) / 2,(windowHeight - height) / 2);
+	frameRate(60);
+	textSize(20);
+	textAlign(CENTER);
 	noStroke();
 	player = new Bubble(25, height-height/10);
 	maxHeight = height-height/10 - 25;
+	p = new Enemy(width);
 
 }
 
 function draw() {
-
+	
 	drawGround();
 	moveBubble();
 	drawCharacter();
-	let p = new Enemy(width);
+	
+	p.move();
+	p.draw();
 	//ellipse(width/2, maxHeight, 15, 15); Debug Ellipse;
 
 }
@@ -28,6 +35,10 @@ function drawGround() {
 	background(0, 125, 255);
 	fill(125);
 	rect(0, height-height/6, width, height/6);
+	push();
+		fill(255);
+		text(round(frameRate()), width-width/30, height-height/1.08);
+	pop();
 
 }
 
@@ -46,4 +57,13 @@ function keyPressed(){
 		player.jump();
 	}
 
+}
+
+function touchStarted() {
+	player.jump();
+	return false;
+}
+
+function touchEnded() {
+	return false;
 }
